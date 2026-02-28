6981fe5cf70294432dfd4824aa7c7cf8a4fb298a
Utkarsh Gayguwal <utkarshgayguwal123@gmail.com>
2026-02-28 14:33:01 +0000

    Added Basic Structure, and players

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,28 @@
-
 function App() {
-  
-
   return (
-    <p>Coming Soon</p>
-  )
+    <>
+      <div id="game-container">
+        <ol id="players">
+          <li>
+            <span className="player">
+              <span className="player-name">Player 1</span>
+              <span className="player-symbo">X</span>
+            </span>
+            <button>Edit</button>
+          </li>
+          <li>
+            <span className="player">
+              <span className="player-name">Player 2</span>
+              <span className="player-symbo">O</span>
+            </span>
+            <button>Edit</button>
+          </li>
+        </ol>
+        GAME BOARD
+      </div>
+      LOGS
+    </>
+  );
 }
 
-export default App
+export default App;
