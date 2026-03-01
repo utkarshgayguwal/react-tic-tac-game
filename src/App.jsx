17750b60d7c7377d6cffa47db780703ce22f3d79
Utkarsh Gayguwal <utkarshgayguwal123@gmail.com>
2026-03-01 21:02:04 +0530

    Added winning_combinations file containing all combinations

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import Player from "./components/Player";
 import GameBoard from "./components/GameBoard";
 import Log from "./components/Log";
+import { WINNING_COMBINATIONS } from "./winning-combinations";
 
 function derivedCurrentPlayer(gameTurns){
   let currentPlayer = "X";
